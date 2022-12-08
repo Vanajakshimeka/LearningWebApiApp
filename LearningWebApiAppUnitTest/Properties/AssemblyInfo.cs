bc32998560dc11145a501fe6411cbc26bdde5eb6
vanajakshimeka <vanajakshim1@gmail.com>
2022-12-08 05:51:58 +0530

    unit test case changes

---
+++ LearningWebApiAppUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LearningWebApiAppUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LearningWebApiAppUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("87c2ac00-a35a-4a67-b327-7013d3c3967d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LearningWebApiAppUnitTest/WebApiUnitTest.cs
@@ -0,0 +1,58 @@
+﻿using LearningWebApiApp.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LearningWebApiAppUnitTest
+{
+    [TestClass]
+    public class WebApiUnitTest
+    {
+        string response;
+
+        /// <summary>
+        /// Method for common variable initialization
+        /// </summary>
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var controller = new ValuesController();
+             response = controller.Get();  // Installed Microsoft.AspNet.WebApi.Core 5.2.7
+        }
+
+        /// <summary>
+        /// Test case for data is not null
+        /// </summary>
+        [TestMethod]
+        public void TestStringNotEmpty()
+        {
+            Assert.IsNotNull(response);
+        }
+
+        /// <summary>
+        /// Test case for string are equal
+        /// </summary>
+        [TestMethod]
+        public void TestMatchingString()
+        {
+            Assert.AreEqual("Sample local API string Data to display in the UI", response);
+        }
+
+        /// <summary>
+        /// Test case for string are not equal
+        /// </summary>
+        [TestMethod]
+        public void TestNonMatchingString()
+        {
+            Assert.AreNotEqual("Test", response);
+        }
+
+        /// <summary>
+        /// Test case for string are not equal
+        /// </summary>
+        [TestMethod]
+        public void TestNonMatchingStringNegative()
+        {
+            Assert.AreEqual("Test", response);
+        }
+    }
+}
